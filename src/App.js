35ee8608f791ef4f63e3396404621f555bff193f
Nitish chaurasiya <nitishchaurasiyac555@gmail.com>
2023-07-03 15:46:51 +0530

    App completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
+import Header from "./components/Header";
+
+import './styles/App.scss'
+import './styles/Header.scss'
+import './styles/Home.scss'
+import './styles/Footer.scss'
+import './styles/Contact.scss'
+import './styles/mediaquery.scss'
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+import Service from "./components/Service";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header/>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route exact path="/contact" element={<Contact/>} />
+          <Route exact path="/service" element={<Service/>} />
+        </Routes>
+        <Footer/>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+            TechStar
+            <p>@All right reserver</p>
+        </div>
+        <div>
+            <h5>Follow Us</h5>
+            <div>
+                <a href="https://youtube.com" target='blank'>Youtube</a>
+                <a href="https://github.com" target='blank'>Github</a>
+                <a href="https://instagram.com target='blank'">Instagram</a>
+            </div>
+        </div>
+      
+    </footer>
+  )
+}
+
+export default Footer
